d672833a20acd3aa4615a9c404ba6cb3f3fcf33c
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2023-12-24 16:24:42 -0300

    chore: create remix project

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Guilherme de Andrade";
-export const SITE_DESCRIPTION = "Ramblings from an unquiet mind";
