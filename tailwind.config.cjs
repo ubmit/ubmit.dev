7b8c16b9b9e92f78fd208948b72143d5f3f296db
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2024-04-22 17:40:06 +0100

    style: use radix colors for gray

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,24 @@
 export default {
   content: ["./src/**/*.{astro,html,md,mdx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          100: "var(--gray-1)",
+          200: "var(--gray-2)",
+          300: "var(--gray-3)",
+          400: "var(--gray-4)",
+          500: "var(--gray-5)",
+          600: "var(--gray-6)",
+          700: "var(--gray-7)",
+          800: "var(--gray-8)",
+          900: "var(--gray-9)",
+          1000: "var(--gray-10)",
+          1100: "var(--gray-11)",
+          1200: "var(--gray-12)",
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
